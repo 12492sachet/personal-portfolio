968a7f8d3340c20d1ca4da21ce6576bcc4ee73ad
lewis machabe <lewismachabe@gmail.com>
2023-11-22 17:00:22 +0300

    Responsive for small devices and X

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -3,7 +3,7 @@ const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
 
 styleSwitcherToggle.addEventListener("click", () => {
   document.querySelector(".style-switcher").classList.toggle("open");
-});
+}); 
 
 
 // Hide style-switcher on scroll
